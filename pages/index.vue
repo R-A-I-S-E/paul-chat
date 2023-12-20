c7bb613edc617c8a5d7fed9f89ce066f189768f2
Paul Eßer <hello@paul.esser.com>
2023-12-20 16:24:59 +0100

    Add prompt templates and handle button click event

--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,11 @@ import { marked } from 'marked';
 import DOMPurify from 'dompurify'
 import type { FunctionCallHandler, Message } from 'ai';
 import type { DatabaseResponse, Input } from '../types';
+//data------------------------------------------------------------------------------------
+const promptTemplates = ref([
+  ["List some tools for sound generation", "to create drum patterns and percussion"], ["Are there any cheap plugins", "for pitch correction and vocal processing"],
+  ["Could you recommend a tool", "for noise reduction"]
+])
 //methods ------------------------------------------------------------------------------------
 const querryDataBase = async (_input: Input) => {
   if (!_input.queryTexts) throw new Error('no input provided')
@@ -19,6 +24,7 @@ const querryDataBase = async (_input: Input) => {
 
   return response?.data?.value?.["response"]["documents"]
 };
+
 const functionCallHandler: FunctionCallHandler = async (
   chatMessages,
   functionCall,
@@ -43,11 +49,31 @@ const functionCallHandler: FunctionCallHandler = async (
   }
 };
 
-const { messages, input, handleSubmit } = useChat({
+const { messages, input, handleSubmit, append } = useChat({
   api: '/api/chat-with-functions',
   experimental_onFunctionCall: functionCallHandler,
+  initialMessages: [
+    {
+      id: nanoid(),
+      role: 'system',
+      content: 'Only aswer questions that relate to the database context! Dont lie and only use information from the database',
+    }, {
+      id: nanoid(),
+      role: 'system',
+      content: 'talk like a all knowing music database, dont say to the user you are searching one. Query more that you need and then filter the results',
+    },
+    {
+      id: nanoid(),
+      role: 'system',
+      content: 'Structure your answers in markdown',
+    }]
 });
-
+const handleClick = (e: Event) => {
+  e.preventDefault()
+  const target = e.target as HTMLButtonElement
+  console.log(target.innerText)
+  append({ content: target.innerText, role: 'user' })
+}
 // Generate a map of message role to text color
 const roleToColorMap: Record<Message['role'], string> = {
   system: 'red',
@@ -56,53 +82,52 @@ const roleToColorMap: Record<Message['role'], string> = {
   assistant: 'green',
   data: 'orange',
 };
-const startSystemMessages = () => {
-  messages.value.push({
-    id: nanoid(),
-    role: 'system',
-    content: 'Only aswer questions that relate to the database context! Dont lie and only use information from the database',
-  });
-  messages.value.push({
-    id: nanoid(),
-    role: 'system',
-    content: 'talk like a all knowing music database, dont say to the user you are searching one. Query more that you need and then filter the results',
-  });
-  messages.value.push({
-    id: nanoid(),
-    role: 'system',
-    content: 'Structure your answers in markdown',
-  });
-}
+
 const onlyUserAndAssistant = computed(() => {
   let filtered = messages.value.filter(m => m.role === 'user' || m.role === 'assistant')
   filtered = filtered.filter(m => !m.function_call)
   return filtered
 })
 const parseMarkdown = (role: string, content: string) => {
-  const combined = `**${role}**: ${content}`
+  const roleString = role === 'user' ? 'YOU' : 'RAISE'
+  const combined = `**${roleString}**: ${content}`
   const markdown = marked.parse(combined) as string
   return DOMPurify.sanitize(markdown)
 }
 
 //lifecycle ------------------------------------------------------------------------------------
-onMounted(async () => {
-  startSystemMessages()
+onMounted(() => {
+
 })
 
 </script>
 
-<template>
-  <div class="flex flex-col w-full max-w-md py-24 mx-auto stretch">
-    <div v-for="m in onlyUserAndAssistant" key="m.id"
-      class="[&>ul]:leading-6 [&>ul]:list-disc [&>ul]:list-inside [&>ol]:leading-6 [&>ol]:list-decimal [&>*]:mb-2"
-      :style="{ color: roleToColorMap[m.role] }" v-html="parseMarkdown(m.role, m.content)">
-
-
-
+<template >
+  <main class=" bg-[#fafafa] h-screen  h-dvh w-full flex flex-row">
+    <header class="fixed top-0 left-0 right-0 h-20 bg-red-500 z-50"> </header>
+    <!-- Starter Window  with TODO: Prompt templates-->
+    <div class="w-1/3 bg-green-500 flex flex-col pt-20">
+      <div class=" mt-20 flex flex-col content-stretch justify-start" id="prompt-templates"
+        v-if="onlyUserAndAssistant.length <= 0">
+        <button v-for="template in promptTemplates" class="pre-wrap text-start mx-20  my-5" @click="handleClick"> <strong
+            class="w-full">{{
+              template[0] }}</strong>
+          <br>
+          {{ template[1] }}
+        </button>
+      </div>
     </div>
-    <form @submit="handleSubmit">
-      <input class="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl" v-model="input"
-        placeholder="Say something..." />
-    </form>
-  </div>
-</template>
+    <!-- Chat History: -->
+    <div class="flex flex-col w-2/3 px-40 pt-60 pb-24 mx-auto stretch overflow-y-scroll">
+      <div v-for="m in onlyUserAndAssistant" key="m.id"
+        class="[&>ul]:leading-6 [&>ul]:list-disc [&>ol]:leading-6 [&>ol]:list-decimal [&>*]:mb-2"
+        :style="{ color: roleToColorMap[m.role] }" v-html="parseMarkdown(m.role, m.content)">
+      </div>
+      <!-- Input -->
+      <form @submit="handleSubmit">
+        <input class="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded" v-model="input"
+          placeholder="Say something..." />
+      </form>
+    </div>
+  </main>
+</template> 
