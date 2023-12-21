b176d3f2b31ad08376ef657d0e4ec54a7321c4d2
Paul Eßer <hello@paul.esser.com>
2023-12-22 00:46:47 +0100

    fix text colors

--- components/Header.vue
+++ components/Header.vue
@@ -7,10 +7,10 @@ const navigation = ref([{ label: "help", route: "help" }, { label: "research", r
         class="bg-gray-100 dark:bg-gray-950 fixed top-0 left-0 right-0 h-20 z-50 border-b-2 border-gray-300 dark:border-gray-700  flex flex-row justify-end px-10">
         <nav class="flex flex-row gap-3 items-center">
             <!-- <nuxt-link v-for="element in navigation" :key="element.label" :to="`/${element.route}`"
-                class="uppercase text-gray-300 dark:text-gray-300 border-2 border-gray-300 dark:border-gray-700 py-2 px-4 rounded-3xl text-lg font-medium">
+                class="uppercase text-gray-300 dark:text-gray-700 border-2 border-gray-300 dark:border-gray-700 py-2 px-4 rounded-3xl text-lg font-medium">
                 {{ element.label }}</nuxt-link> -->
             <button v-for="element in navigation" :key="element.label" :to="`/${element.route}`"
-                class="uppercase text-gray-300 dark:text-gray-300 border-2 border-gray-300 dark:border-gray-700 py-2 px-4 rounded-3xl text-lg font-medium">
+                class="uppercase text-gray-300 dark:text-gray-700 border-2 border-gray-300 dark:border-gray-700 py-2 px-4 rounded-3xl text-lg font-medium">
                 {{ element.label }}</button>
         </nav>
     </header>
