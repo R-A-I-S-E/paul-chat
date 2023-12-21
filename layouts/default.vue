5d17791ab8179b1cb7a4282dc1173b1dc733a121
Paul Eßer <hello@paul.esser.com>
2023-12-22 00:44:07 +0100

    fix height

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <Header />
-    <main class=" bg-gray-100 dark:bg-gray-950 h-screen  h-dvh w-full flex flex-row">
+    <main class=" bg-gray-100 dark:bg-gray-950 min-h-screen  min-h-dvh  w-full flex flex-row">
         <slot />
     </main>
 </template>
