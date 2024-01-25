13ce26712f1cf6e43909919b8c90fe833939d50c
paulesser <hello@paul-esser.com>
2024-01-25 17:39:11 +0100

    Add @nuxtjs/kinde for auth (#6)
    * Add @nuxtjs/kinde for auth
    
    * add sign out
    
    ---------
    
    Co-authored-by: Paul Eßer <hello@paul.esser.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', '@nuxtjs/kinde'],
   nitro: {
     preset: 'vercel-edge', // you can use 'vercel' or other providers here
   },
---
+++ pages/login/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <div v-if="!$auth.loggedIn" class="flex justify-center w-full h-full items-center">
+    <NuxtLink
+      to="/api/login" external class="uppercase text-gray-300 dark:text-gray-700 border-2 border-gray-300 dark:border-gray-700 py-2 px-4 rounded-3xl text-xl font-medium mr-4"
+    >
+      Sign in
+    </NuxtLink>
+    <NuxtLink to="/api/register" external class="uppercase text-gray-300 dark:text-gray-700 border-2 border-gray-300 dark:border-gray-700 py-2 px-4 rounded-3xl text-xl font-medium">
+      Sign up
+    </NuxtLink>
+  </div>
+</template>
