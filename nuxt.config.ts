d879118ddf6f2d9536ed434f32b985551447f87c
paulesser <hello@paul.esser.com>
2024-02-14 15:17:48 +0100

    add nuxt prefix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', ],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
   nitro: {
     preset: 'vercel-edge', // you can use 'vercel' or other providers here
   },
   runtimeConfig: {
-    openaiApiKey: process.env.OPENAI_API_KEY,
-    chromadbUrl: process.env.CHROMADB_URL,
-    chromadbCollectionName: process.env.CHROMADB_COLLECTION,
-    discordWebhookUrl: process.env.DISCORD_WEBHOOK_URL,
+    openaiApiKey: process.env.NUXT_OPENAI_API_KEY,
+    chromadbUrl: process.env.NUXT_CHROMADB_URL,
+    chromadbCollectionName: process.env.NUXT_CHROMADB_COLLECTION,
+    discordWebhookUrl: process.env.NUXT_DISCORD_WEBHOOK_URL,
   },
   googleFonts: {
     families: {
