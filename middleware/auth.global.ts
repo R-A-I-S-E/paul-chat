d6119af5871c076626bc951128b0267e3600c63d
paulesser <hello@paul.esser.com>
2024-02-15 22:42:11 +0100

    fix weird redirect issue

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,12 +1,15 @@
+let allowed = false
+
 export default defineNuxtRouteMiddleware(async (to) => {
   const client = useKindeClient()
-  const p = await useAsyncData(async () => {
-    const { permissions } = (await client.getPermissions()) ?? {}
-    return permissions
+  const { data: access } = await useAsyncData(async () => {
+    return (await client?.getPermission('useChat')) ?? {}
   })
-  const permission = p?.data?.value?.includes('useChat') || false
-  if (!permission && to.path !== '/login')
+  if (!allowed)
+    allowed = access.value.isGranted || false
+
+  if (!allowed && to.fullPath !== '/login')
     return navigateTo('/login')
-  else if (permission && to.path === '/login')
+  if (allowed && to.fullPath === '/login')
     return navigateTo('/')
 })
