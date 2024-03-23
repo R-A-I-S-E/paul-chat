4dc87e16946fc324bdce3e6f9342ec9d1494dc22
paulesser <hello@paul.esser.com>
2024-03-23 21:25:13 +0100

    remove kinde

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts', '@nuxtjs/kinde'],
-  nitro: {
-    preset: 'vercel-edge', // you can use 'vercel' or other providers here
-  },
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
   runtimeConfig: {
     openaiApiKey: process.env.NUXT_OPENAI_API_KEY,
-    chromadbUrl: process.env.NUXT_CHROMADB_URL,
-    chromadbCollectionName: process.env.NUXT_CHROMADB_COLLECTION,
+    chromadbUrl: process.env.NUXT_CHROMADB_URL || 'http://localhost:8000',
+    chromadbAuth: process.env.NUXT_CHROMADB_AUTH,
+    chromadbCollectionName: process.env.NUXT_CHROMADB_COLLECTION || 'pluginList',
     discordWebhookUrl: process.env.NUXT_DISCORD_WEBHOOK_URL,
   },
   googleFonts: {
@@ -17,12 +15,5 @@ export default defineNuxtConfig({
       'Archivo Black': [400],
     },
   },
-  kinde: {
-    authDomain: process.env.NUXT_KINDE_AUTH_DOMAIN,
-    clientId: process.env.NUXT_KINDE_CLIENT_ID,
-    clientSecret: process.env.NUXT_KINDE_CLIENT_SECRET,
-    redirectURL: process.env.NUXT_ENV_VERCEL_URL ? `https://${process.env.NUXT_ENV_VERCEL_URL}/api/callback` : `http://localhost:3000/api/callback`,
-    logoutRedirectURL: process.env.NUXT_ENV_VERCEL_URL ? `https://${process.env.NUXT_ENV_VERCEL_URL}` : `http://localhost:3000`,
-    postLoginRedirectURL: process.env.NUXT_ENV_VERCEL_URL ? `https://${process.env.NUXT_ENV_VERCEL_URL}` : `http://localhost:3000`,
-  },
+
 })
