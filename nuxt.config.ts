ac86c53f7e51e05e963a1c41c297ac3c9f531f4b
paulesser <hello@paul.esser.com>
2024-03-24 22:49:26 +0100

    add task

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,17 +2,29 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
+  nitro: {
+    experimental: {
+      tasks: true,
+    },
+    scheduledTasks: {
+
+      '* * * * *': ['chroma:update'],
+    },
+
+  },
   runtimeConfig: {
     openaiApiKey: process.env.NUXT_OPENAI_API_KEY,
     chromadbUrl: process.env.NUXT_CHROMADB_URL || 'http://localhost:8000',
-    chromadbAuth: process.env.NUXT_CHROMADB_AUTH,
+    chromadbAuth: process.env.NUXT_CHROMADB_AUTH || process.env.CHROMA_SERVER_AUTH_CREDENTIALS,
     chromadbCollectionName: process.env.NUXT_CHROMADB_COLLECTION || 'pluginList',
     discordWebhookUrl: process.env.NUXT_DISCORD_WEBHOOK_URL,
+    googleAPIKey: process.env.NUXT_GOOGLE_API_KEY,
+    spreadsheetId: process.env.NUXT_SPREADSHEET_ID,
   },
   googleFonts: {
     families: {
       'IBM Plex Sans': [400, 500, 600, 700],
-      'Archivo Black': [400],
+      'Archivo Black': 400,
     },
   },
 
