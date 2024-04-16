dda9d8c38853c8122ce60ff2634a8c6b10325e8a
paulesser <hello@paul-esser.com>
2024-04-16 22:27:39 +0200

    remove bug-report (#10)
    Co-authored-by: paulesser <hello@paul.esser.com>

--- components/Header.vue
+++ components/Header.vue
@@ -1,27 +1,8 @@
-<script setup lang="ts">
-const route = useRoute()
-const navigation = computed(() => {
-  if (route.fullPath === '/bug-report')
-    return [{ label: 'chat', route: '' }]
-  else
-    return [{ label: 'bug report', route: 'bug-report' }]
-})
-</script>
-
 <template>
   <header
     class="bg-gray-100 dark:bg-gray-950 fixed top-0 left-0 right-0 h-20 z-50 border-b-2 border-gray-300 dark:border-gray-700  flex flex-row justify-end px-10"
   >
     <nav class="flex flex-row gap-3 items-center">
-      <!-- <nuxt-link v-for="element in navigation" :key="element.label" :to="`/${element.route}`"
-                class="uppercase text-gray-300 dark:text-gray-700 border-2 border-gray-300 dark:border-gray-700 py-2 px-4 rounded-3xl text-xl font-medium">
-                {{ element.label }}</nuxt-link> -->
-      <NuxtLink
-        v-for="element in navigation" :key="element.label" :to="`/${element.route}`"
-        class="uppercase text-gray-300 dark:text-gray-700 border-2 border-gray-300 dark:border-gray-700 py-2 px-4 rounded-3xl text-xl font-medium"
-      >
-        {{ element.label }}
-      </NuxtLink>
       <a href="https://www.hfg-offenbach.de/de/pages/raise-ki-forschung-sound-und-musik#ueber" target="_blank" class="uppercase text-gray-300 dark:text-gray-700 border-2 border-gray-300 dark:border-gray-700 py-2 px-4 rounded-3xl text-xl font-medium">about</a>
     </nav>
   </header>
